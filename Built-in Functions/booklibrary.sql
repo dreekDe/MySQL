59bd43d8fb0dbc5fbdcc0bc823f91bf1b9f891a7
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-03-27 16:42:23 +0200

    Update booklibrary.sql

--- Built-in Functions/booklibrary.sql
+++ Built-in Functions/booklibrary.sql
@@ -7,3 +7,21 @@ WHERE
 ORDER BY `id`;
 
 
+
+SELECT 
+    REPLACE(`title`, 'The', '***') AS `title`
+FROM
+    `books`
+WHERE
+    SUBSTRING(`title`, 1, 3) = 'The'
+ORDER BY `id`;
+
+
+
+SELECT 
+    ROUND(SUM(`cost`), 2)
+FROM
+    `books`;
+
+
+
