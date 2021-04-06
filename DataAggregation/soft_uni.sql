eb72714dbef0def9a8cd07967389203a3f98314e
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-04-06 13:46:28 +0300

    Add files via upload

---
+++ DataAggregation/soft_uni.sql
@@ -0,0 +1,39 @@
+SELECT 
+    `department_id`, MIN(`salary`) AS `minimum_salary`
+FROM
+    `employees`
+WHERE
+    `department_id` IN (2 , 5, 7)
+        AND `hire_date` > '2000-01-01'
+GROUP BY `department_id`
+ORDER BY `department_id`;
+
+
+
+
+
+CREATE TABLE `temp` SELECT * FROM
+    `employees`
+WHERE
+    `salary` > 30000;
+    
+DELETE FROM `temp` 
+WHERE
+    `manager_id` = 42;
+    
+UPDATE `temp` 
+SET 
+    `salary` = `salary` + 5000
+WHERE
+    `department_id` = 1;
+    
+
+SELECT 
+    `department_id`, avg(`salary`) as `avg_salary`
+FROM
+    `temp`
+    GROUP BY `department_id`
+    ORDER BY `department_id`;
+    
+  
+    
