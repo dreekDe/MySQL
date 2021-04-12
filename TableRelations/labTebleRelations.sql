63e4a4e66b1623256deb67fde644e36432f8610c
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-04-12 15:47:35 +0300

    Update labTebleRelations.sql

--- TableRelations/labTebleRelations.sql
+++ TableRelations/labTebleRelations.sql
@@ -38,12 +38,25 @@ FROM
   
   
   
+  DROP TABLES `peaks`,`mountains`;
   
+  CREATE TABLE `mountains` (
+    `id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL
+
+);
+
+CREATE TABLE `peaks` (
+    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `mountain_id` INT(11),
+    CONSTRAINT `fk_peaks_mountains` FOREIGN KEY (`mountain_id`)
+        REFERENCES `mountains` (`id`)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
 
  
      
-     
-     
-     
-     
-     
+ 
