3ee49fb654a435b3d3347b34efaddcdb17311e10
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-04-12 17:45:11 +0300

    Update ex-tableRelation.sql

--- TableRelations/ex-tableRelation.sql
+++ TableRelations/ex-tableRelation.sql
@@ -5,20 +5,18 @@ CREATE TABLE `people` (
     `person_id` INT PRIMARY KEY AUTO_INCREMENT,
     `first_name` VARCHAR(50) NOT NULL,
     `salary` DECIMAL(11 , 2 ),
-    `passrport_id` INT UNIQUE NOT NULL
+    `passrport_id` INT UNIQUE
 );
 
 CREATE TABLE `passports` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `passport_number` VARCHAR(30) UNIQUE NOT NULL
+    `passport_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `passport_number` VARCHAR(30) UNIQUE 
 );
 
 ALTER TABLE `people`
 ADD CONSTRAINT `fk_people_passport`
-FOREIGN KEY (`passrport_id`)
-REFERENCES `passports`(`id`)
-ON DELETE CASCADE
-ON UPDATE CASCADE;
+FOREIGN KEY `people`(`passrport_id`)
+REFERENCES `passports`(`passport_id`);
 
 INSERT INTO `passports`
 VALUE 
@@ -28,8 +26,12 @@ VALUE
 
 INSERT INTO `people` 
 VALUE 
-(1, 'Roberto', 43300.00, 102),
-(2, 'Tom', 56100.00, 103),
+(1,  'Roberto', 43300.00, 102),
+(2,	'Tom', 56100.00, 103),
 (3, 'Yana', 60200.00, 101);
 
 DROP SCHEMA `now`;
+
+
+
+
