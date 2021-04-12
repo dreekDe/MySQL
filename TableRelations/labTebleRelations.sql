d5aa20f4ea9c8d34f46dfbdb92525108855b40de
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-04-12 15:38:14 +0300

    Update labTebleRelations.sql

--- TableRelations/labTebleRelations.sql
+++ TableRelations/labTebleRelations.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `mountains` (
     `id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(50)
+    `name` VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE `peaks` (
@@ -10,3 +10,40 @@ CREATE TABLE `peaks` (
     CONSTRAINT `fk_peaks_mountains` FOREIGN KEY (`mountain_id`)
         REFERENCES `mountains` (`id`)
 );
+
+
+
+
+SELECT 
+    v.`driver_id`,
+    v.`vehicle_type`,
+    CONCAT_WS(' ', c.first_name, c.last_name) AS `driver_name`
+FROM
+    `campers` AS c
+        JOIN
+    `vehicles` AS v ON c.`id` = v.`driver_id`;
+    
+    
+  
+  
+SELECT 
+    r.`starting_point` AS `route_starting_point`,
+    r.`end_point` AS `route_ending_point`,
+    r.`leader_id`,
+    CONCAT_WS(' ', c.`first_name`, c.`last_name`) AS `leader_name`
+FROM
+    `routes` AS r
+        JOIN
+    `campers` AS c ON r.`leader_id` = c.`id`; 
+  
+  
+  
+  
+
+ 
+     
+     
+     
+     
+     
+     
