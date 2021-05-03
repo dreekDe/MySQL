89c88f078c66f1d7e7921170cc853acc94a67c73
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-05-03 15:46:43 +0300

    Update Database Programmability and Transactions-lab.sql

--- Database Programmability and Transactions/Database Programmability and Transactions-lab.sql
+++ Database Programmability and Transactions/Database Programmability and Transactions-lab.sql
@@ -56,5 +56,12 @@ END IF;
 END
                                                                                            
                                                                                            
+#4
+CREATE DEFINER=`root`@`localhost` TRIGGER `tr_deleted_employees` AFTER DELETE ON `deleted_employees` FOR EACH ROW BEGIN
+INSERT INTO deleted_employees 
+    (first_name, last_name, middle_name, job_title, department_id, salary)
+    VALUES (OLD.first_name, OLD.last_name, OLD.middle_name,
+    OLD.job_title, OLD.department_id, OLD.salary);
+END
                                                                                            
     
