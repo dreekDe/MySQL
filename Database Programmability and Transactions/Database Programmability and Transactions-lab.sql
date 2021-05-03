b1e924c071d911ce1c861eafb1c86cb37a6e9ee9
dreekDe <70441036+dreekDe@users.noreply.github.com>
2021-05-03 14:15:52 +0300

    Update Database Programmability and Transactions-lab.sql

--- Database Programmability and Transactions/Database Programmability and Transactions-lab.sql
+++ Database Programmability and Transactions/Database Programmability and Transactions-lab.sql
@@ -1,2 +1,26 @@
+#1
+CREATE DEFINER=`root`@`localhost` FUNCTION `ufn_count_employees_by_town`(town_name VARCHAR(45)) RETURNS int
+    DETERMINISTIC
+BEGIN
+
+DECLARE count_town_ppl INT (11);
+
+SET  `count_town_ppl` := (SELECT COUNT(e.employee_id) FROM employees AS e
+       INNER JOIN `addresses` AS a 
+        ON a.address_id = e.address_id
+      INNER  JOIN `towns` AS t 
+        ON a.town_id = t.town_id
+WHERE  t.`name` = town_name); 
+
+RETURN  count_town_ppl;
+END
+
+                                                                                           
 SELECT UFN_COUNT_EMPLOYEES_BY_TOWN('Sofia') AS `count`;
+                                                                                           
+
+#2
+                                                                                           
+                                                                                           
+                                                                                    
     
